b3de674c357ba4a081ef7dfefbeefa8dfeb3f01f
souljahwic202828 <85344237+souljahwic202828@users.noreply.github.com>
2021-07-02 02:31:50 +0300

    Update hideit.py

--- userbot/plugins/hideit.py
+++ userbot/plugins/hideit.py
@@ -1,19 +1,26 @@
+# Template by @Infinity20998
+# Modified by @o_s_h_o_r_a_j
 from userbot import catub
-from ..core.managers import edit_delete, edit_or_reply
+
+from ..core.managers import edit_delete
 from ..helpers.utils import reply_id
 
 plugin_category = "extra"
 
 
 @catub.cat_cmd(
-    pattern="hde ?(.*)",
-    command=("hde", plugin_category),
+    pattern="hc ?(.*)",
+    command=("hc", plugin_category),
     info={
-        "header": "Hides the message via @hideitbot",
-        "usage": "{tr}hde <text>",
+        "header": "Hides the message via @hideitbot with █",
+        "usage": [
+            "{tr}hc <text>",
+            "{tr}hc -p <text *hide*> "
+        ],
+        "flag":{"-p": "hides words inside **"},
     },
 )
-async def hideit(event):
+async def hideit01(event):
     "Hide your message through @HideitBot"
     if event.fwd_from:
         return
@@ -21,9 +28,40 @@ async def hideit(event):
     hidetxt = event.pattern_match.group(1)
     reply_to_id = await reply_id(event)
     if not hidetxt:
-            return await edit_delete(
-                event, "__What should I hide through bot? Give some text.__"
-            )
-    results = await event.client.inline_query(bot, hidetxt)
-    await results[0].click(event.chat_id, reply_to=reply_to_id)
+        return await edit_delete(
+            event, "__What should I hide through bot? Give some text.__"
+        )
     await event.delete()
+    if "-p" in hidetxt:
+        hidetxt = hidetxt.replace("-p", "")
+        results = await event.client.inline_query(bot, hidetxt)
+        await results[1].click(event.chat_id, reply_to=reply_to_id)
+    else:
+        results = await event.client.inline_query(bot, hidetxt)
+        await results[0].click(event.chat_id, reply_to=reply_to_id)
+
+
+@catub.cat_cmd(
+    pattern="hq ?(.*)",
+    command=("hq", plugin_category),
+    info={
+        "header": "Hides the message via @hideitbot with ❓",
+        "usage": [
+            "{tr}hq <text>",
+        ],
+    },
+)
+async def hideit02(event):
+    "Hide your message through @HideitBot"
+    if event.fwd_from:
+        return
+    bot = "@hideitbot"
+    hidetxt = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if not hidetxt:
+        return await edit_delete(
+            event, "__What should I hide through bot? Give some text.__"
+        )
+    await event.delete()
+    results = await event.client.inline_query(bot, hidetxt)
+    await results[1].click(event.chat_id, reply_to=reply_to_id)
